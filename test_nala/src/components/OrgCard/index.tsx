b9be8cd522e6daa5b83126f3f8335cdfac57e581
Ernesto <enm2071@gmail.com>
2025-02-19 16:26:05 -0400

    Update OrgCard component with new icons and styles

--- test_nala/src/components/OrgCard/index.tsx
+++ test_nala/src/components/OrgCard/index.tsx
@@ -3,10 +3,12 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
-import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 import Typography from '@mui/material/Typography';
 import { Checkbox, IconButton } from "@mui/material";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SaveIcon from '@mui/icons-material/Save';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 type OrgCardProps = {
     title: string;
@@ -17,37 +19,71 @@ const OrgCard = (props: OrgCardProps) => {
     const { title } = props;
     const [checked, setChecked] = React.useState(true);
     return (
-        <Card sx={{ minWidth: 275 }}>
-            <CardHeader
-                avatar={<Checkbox {...label} checked={checked} onChange={() => setChecked(!checked)}
-                />}
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                }
-            />
-            <CardContent>
-                <Typography gutterBottom sx={{
-                    color: 'text.secondary',
-                    fontSize: 14
-                }}>
-                    {title}
-                </Typography>
-                <Typography variant="h5" component="div">
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            gap: 10,
+        }}>
+            <Card sx={{
+                minWidth: 325,
+                minHeight: 350,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+            }}>
+                <CardHeader
+                    avatar={<Checkbox {...label} checked={checked} onChange={() => setChecked(!checked)}
+                    />}
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                />
+                <CardContent>
+                    <Typography gutterBottom sx={{
+                        color: 'text.secondary',
+                        fontSize: 24,
+                        fontWeight: 'bold',
+                        lineHeight: 1.2,
+                        letterSpacing: '0.1em',
+                    }}>
+                        {title}
+                    </Typography>
+                    <Typography variant="h5" component="div">
 
-                </Typography>
-                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-                <Typography variant="body2">
-                    well meaning and kindly.
-                    <br />
-                    {'"a benevolent smile"'}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+                    <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                    </Typography>
+                </CardContent>
+                <CardActions sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    borderTop: '1px solid #f0f0f0',
+                    boxShadow: '0px -1px 0px #f0f0f0',
+                    backgroundColor: '#DBE2E9'
+                }}>
+                    <IconButton aria-label="save">
+                        <SaveIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+
+                </CardActions>
+            </Card>
+            <IconButton aria-label="add">
+                <AddCircleOutlineIcon color="primary" sx={{
+                    fontSize: 50
+                }} />
+            </IconButton>
+        </div>
     )
 
 }
